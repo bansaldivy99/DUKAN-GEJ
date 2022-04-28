4ed1b0b72021aef13421368da1d64be78f68d0ea
Divy Bansal <divy234@gmail.com>
2022-04-28 23:30:07 +0530

    -Added Sort according to gauge

--- BB/BACKUP/1.py
+++ BB/BACKUP/1.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 import csv 
 
-from operator import itemgetter
-
 df = pd.read_csv('DUKAN_GEJ.csv')
 
 place = df.Place.unique() #Print Different Places
@@ -28,14 +26,19 @@ C =[]
 H =[]
 RD =[]
 B=[]
-X=[] #pata nahi kiska piece hai
-WO=[] #these piece not in caltalogue 
-FullOrder=[] #This list has the list of full order
+X=[] #pata nahi kiska hai
+WO=[] #these piece not in caltalogue
 
 s=set()
 for i in range(0,len(df.axes[0])):
 	s.add(df.iloc[i,0])
 #print(s) #Set of all the design Numbers in stock
+
+C.append(['S_No','Gauge','Meena/sada'])
+H.append(['S_No','Gauge','Meena/sada'])
+RD.append(['S_No','Gauge','Meena/sada'])
+B.append(['S_No','Gauge','Meena/sada'])
+X.append(['S_No','Gauge','Meena/sada'])
 
 for i in range(0,len(order)) :
 
@@ -44,49 +47,30 @@ for i in range(0,len(order)) :
 
 			if df.iloc[row,1] == 'C':
 				C.append([df.iloc[row,0],df.iloc[row,2],df.iloc[row,3]])
-				FullOrder.append([df.iloc[row,0],df.iloc[row,2],df.iloc[row,3]])
 				break
 			elif df.iloc[row,1] == 'H':
 				H.append([df.iloc[row,0],df.iloc[row,2],df.iloc[row,3]])
-				FullOrder.append([df.iloc[row,0],df.iloc[row,2],df.iloc[row,3]])
 				break
 			elif df.iloc[row,1] == 'RD':
 				RD.append([df.iloc[row,0],df.iloc[row,2],df.iloc[row,3]])
-				FullOrder.append([df.iloc[row,0],df.iloc[row,2],df.iloc[row,3]])
 				break
 			elif df.iloc[row,1] == 'B':
 				B.append([df.iloc[row,0],df.iloc[row,2],df.iloc[row,3]])
-				FullOrder.append([df.iloc[row,0],df.iloc[row,2],df.iloc[row,3]])
 				break
 			elif df.iloc[row,1] == 'X':
 				X.append([df.iloc[row,0],df.iloc[row,2],df.iloc[row,3]])
-				FullOrder.append([df.iloc[row,0],df.iloc[row,2],df.iloc[row,3]])
 				break	
 			else :
 				WO.append([df.iloc[row,0]])
 				break
 
-C.sort(key=lambda x: (x[1],x[0]))  # Sorting According to Gauge each List
-H.sort(key=lambda x: (x[1],x[0]))
-RD.sort(key=lambda x: (x[1],x[0]))
-B.sort(key=lambda x: (x[1],x[0]))
-X.sort(key=lambda x: (x[1],x[0]))
-FullOrder.sort(key=lambda x: (x[1],x[0]))
-
-
-C.insert(0,['S_No','Gauge','Meena/sada'])
-H.insert(0,['S_No','Gauge','Meena/sada'])
-RD.insert(0,['S_No','Gauge','Meena/sada'])
-B.insert(0,['S_No','Gauge','Meena/sada'])
-X.insert(0,['S_No','Gauge','Meena/sada'])
-FullOrder.insert(0,['S_No','Gauge','Meena/sada'])
-
-print(C)
 print(H)
 print(RD)
 print(B)
 print(X)
 print(WO)
+
+
 
 with open("order.csv","w+") as my_csv:
     csvWriter = csv.writer(my_csv,delimiter=',')
@@ -103,33 +87,4 @@ with open("order.csv","w+") as my_csv:
     csvWriter.writerows(X)
     my_csv.write("\nWrong Order \n")
     csvWriter.writerows(WO)
-    my_csv.write(" \n\n\nFULL Order \n\n")
-    csvWriter.writerows(FullOrder)
 # Cases like 1,,*,* Wont show up
-
-''' TEST CASE
-10
-11
-1
-14
-19
-20
-21
-48
-39
-251
-1082
-1051
-1405
-1201
-1401
-1402
-1440
-1442
-2140
-2149
-2146
-4
-5
-6
-'''
